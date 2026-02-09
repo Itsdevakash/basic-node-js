30c51707874f5a68e763340c42974a336fef60fb
Itsdevakash <mauryaakash342@gmail.com>
2026-02-10 00:18:16 +0530

    website

--- website.js
+++ website.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 http.createServer((req,resp)=>{
 
 let collectheader = fs.readFileSync("html/header.html","utf-8");
+let collectfooter = fs.readFileSync("html/footer.html","utf-8");
 //   fs.readFile("html/header.html",'utf-8',(err,headerdata)=>{
 //     if(err){
 //         resp.writeHead(500,{"content-type":"text/plain"})
@@ -31,7 +32,7 @@ if(req.url !="/"){
         return false
     }
 
-    resp.write(collectheader+""+data);
+    resp.write(collectheader+""+data+collectfooter);
     resp.end();
 
   })
